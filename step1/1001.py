d57bb43d30e7164fc7e373ac63210eda05797a7a
devraphy <devraphy@gmail.com>
2021-07-28 15:35:08 +0900

    first step

---
+++ step1/1001.py
@@ -0,0 +1,5 @@
+# 백준 단계별 풀이 1단계 
+# https://www.acmicpc.net/problem/1001
+
+a, b = map(int, input().split())
+print(a - b)
---
+++ step1/10171.py
@@ -0,0 +1,4 @@
+# 백준 단계별 풀이 1단계 (10171번)
+# https://www.acmicpc.net/problem/10171
+
+print("\    /\\\n )  ( ')\n(  /  )\n \(__)|")
