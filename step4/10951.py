61a3cf0644987c84cb70cbfca31f3f5413ce04d6
devraphy <devraphy@gmail.com>
2021-07-29 14:49:09 +0900

    step4

---
+++ step4/10951.py
@@ -0,0 +1,12 @@
+# 백준 단계별 풀이 4단계
+# https://www.acmicpc.net/problem/10951
+
+import sys
+
+while(True):
+  try:
+    a, b = map(int, sys.stdin.readline().split())
+    print(a + b)
+  except:
+    break
+  
