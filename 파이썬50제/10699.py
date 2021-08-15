4fe9dde97edb651a35f0f3f68a81c320d5a0f3bb
devraphy <devraphy@gmail.com>
2021-08-15 19:26:19 +0900

    basic_python_50 direcotry name is changed

--- 파이썬50제/10699.py
+++
@@ -1,5 +0,0 @@
-# 파이썬 50제 (1 ~ 50) - 오늘 날짜
-# https://www.acmicpc.net/problem/10699
-
-import datetime
-print(datetime.date.today())
--- 파이썬50제/2163.py
+++
@@ -1,6 +0,0 @@
-# 파이썬 50제 (1 ~ 50) - 초콜릿 쪼개기  
-# https://www.acmicpc.net/problem/2163
-
-# n * m 초콜릿을 1 * 1로 쪼개는 규칙은 n * m -1 이다. 
-n, m = map(int, input().split())
-print(n * m -1)
