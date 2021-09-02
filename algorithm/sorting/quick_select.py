7d2ee178f7ad4c2af865cb476e1eb5dec7eeb09b
devraphy <devraphy@gmail.com>
2021-09-02 21:43:18 +0900

    algorithm_sorting

--- algorithm/sorting/quick_select.py
+++ algorithm/sorting/quick_select.py
@@ -56,6 +56,6 @@ def quick_select(case: List[int], k: int) -> int:
 
   elif length - k < left_idx: # pivot의 좌측에 있는 수 중에 찾고 있는 수가 있는 경우
     return quick_select(case = case[:left_idx], k = k - (length - left_idx)) # 좌측의 요소를 대상으로 quick select를 다시 적용
-    # k - (length - left_idx)를 하는 이유는, 좌측의 수들만을 대상으로 하면 index 번호가 달라지기 때문이다. 
+    # k - (length - left_idx)를 하는 이유는, 좌측의 수 만을 대상으로 배열을 형성하는 경우, 요소 개수의 변화로 타겟 index가 달라지기 때문이다. 
 
 print(quick_select(case = [5, 7, 9, 3, 2, 1, 4], k = 2))
