173b210895966ca9f0e8f023abd8059eaefcc320
devraphy <devraphy@gmail.com>
2021-09-08 01:42:25 +0900

    practicing sorting algorithm

--- memo.py
+++ memo.py
@@ -1,8 +1,7 @@
 from typing import List
-import random
 
 # Bubble Sort
-def bubble_sort(case: List[int]) -> List[int]:
+def bubble_sort(case:List[int]) -> List[int]:
   for idx in range(len(case) - 1):
     for i in range(len(case) - idx - 1):
       if case[i] > case[i + 1]:
@@ -10,7 +9,7 @@ def bubble_sort(case: List[int]) -> List[int]:
   return case
 
 # Insertion Sort
-def insertion_sort(case: List[int]) -> List[int]:
+def insertion_sort(case:List[int]) -> List[int]:
   for idx in range(1, len(case)):
     flag = idx - 1
     current = case[idx]
@@ -19,6 +18,7 @@ def insertion_sort(case: List[int]) -> List[int]:
       flag -= 1
     case[flag + 1] = current
   return case
+
 
 # Selection Sort
 def selection_sort(case: List[int]) -> List[int]:
@@ -33,17 +33,20 @@ def selection_sort(case: List[int]) -> List[int]:
   return case
 
 # Merge Sort
-def merge_sort(case: List[int]) -> List[int]:
+def merge_sort(case:List[int]) -> List[int]:
   length = len(case)
   if length == 1:
     return case
+  
   mid = length // 2
   left_case = case[:mid]
   right_case = case[mid:]
+
   sorted_left = merge_sort(left_case)
   sorted_right = merge_sort(right_case)
+
   sorted_case = []
-  left_idx = 0 
+  left_idx = 0
   right_idx = 0
 
   while left_idx < len(sorted_left) or right_idx < len(sorted_right):
@@ -57,20 +60,17 @@ def merge_sort(case: List[int]) -> List[int]:
       left_idx += 1
       continue
 
-    if sorted_right[right_idx] <= sorted_left[left_idx]:
-      sorted_case.append(sorted_right[right_idx])
-      right_idx += 1
-
-    else:
+    if sorted_left[left_idx] <= sorted_right[right_idx]:
       sorted_case.append(sorted_left[left_idx])
       left_idx += 1
-
+    else:
+      sorted_case.append(sorted_right[right_idx])
+      right_idx += 1
   return sorted_case
-
+   
 # Quick Select
 import random
-
-def quick_select(case: List[int], k: int) -> int:
+def quick_select(case:List[int], k:int) -> int:
   length = len(case)
   if length == 1:
     return case[0]
@@ -78,7 +78,7 @@ def quick_select(case: List[int], k: int) -> int:
   pivot_idx = random.randrange(length)
   last_idx = length - 1
   case[pivot_idx], case[last_idx] = case[last_idx], case[pivot_idx]
-  
+
   left_idx = 0
   right_idx = length - 2
   pivot = case[-1]
@@ -92,22 +92,62 @@ def quick_select(case: List[int], k: int) -> int:
       right_idx -= 1
       continue
 
-    if pivot < case[left_idx] and pivot > case[right_idx]:
+    if pivot < case[left_idx] and pivot >= case[right_idx]:
       case[left_idx], case[right_idx] = case[right_idx], case[left_idx]
+      continue
 
-  case[left_idx], case[last_idx] = case[last_idx], case[left_idx]
+  case[pivot_idx], case[left_idx] = case[left_idx], case[pivot_idx]
 
   if left_idx == length - k:
     return case[left_idx]
-  elif left_idx < length - k:
-    return quick_select(case[left_idx + 1:], k)
-  elif left_idx > length - k:
-    return quick_select(case[:left_idx], k - (length - left_idx))
+  
+  if left_idx < length - k:
+    return quick_select(case = case[left_idx + 1 :], k = k)
+  
+  if left_idx > length - k:
+    return quick_select(case = case[:left_idx], k = k - (length - left_idx))
 
-print(bubble_sort(case = [5, 7, 3, 1, 6, 8, 2, 4]))
-print(insertion_sort(case = [5, 7, 3, 1, 6, 8, 2, 4]))
-print(selection_sort(case = [5, 7, 3, 1, 6, 8, 2, 4]))
-print(merge_sort(case = [5, 7, 3, 1, 6, 8, 2, 4]))
-print(quick_select(case = [5, 7, 3, 1, 6, 8, 2, 4], k = 2))
+print(bubble_sort(case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1]))
+print(insertion_sort(case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1]))
+print(selection_sort(case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1]))
+print(merge_sort(case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1]))
+print(quick_select(case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1], k = 2))
+  
+
+# Quick Sort
+def quick_sort(case:List[int], begin_idx:int, last_idx:int) -> List[int]:
+  length = last_idx - begin_idx + 1
+  if length <= 1:
+    return case
+  
+  pivot_idx = random.randrange(begin_idx, last_idx + 1)
+  case[pivot_idx], case[last_idx] = case[last_idx], case[pivot_idx]
+
+  left_idx = 0
+  right_idx = last_idx - 1
+  pivot = case[last_idx]
+
+  while left_idx <= right_idx:
+    if case[left_idx] <= pivot:
+      left_idx += 1
+      continue
+
+    if case[right_idx] > pivot:
+      right_idx -= 1
+      continue
+
+    if pivot < case[left_idx] and pivot >= case[right_idx]:
+      case[left_idx], case[right_idx] = case[right_idx], case[left_idx]
+      continue
+
+  case[last_idx], case[left_idx] = case[left_idx], case[last_idx]
+
+  quick_sort(case = case, begin_idx = left_idx + 1, last_idx = last_idx)
+  quick_sort(case = case, begin_idx = begin_idx, last_idx = left_idx - 1)
+  return case
+
+case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1]
+quick_sort(case = case, begin_idx = 0, last_idx = len(case) - 1)
+print(case)
 
 
