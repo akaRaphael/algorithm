6b142f5a6271a5924f17703cfc515c536cb3461c
devraphy <devraphy@gmail.com>
2021-09-08 19:26:06 +0900

    algorithm_sorting_heap_sort

---
+++ algorithm/sorting/heap_sort.py
@@ -0,0 +1,31 @@
+# Heap Sort
+
+# 1) Heap Sort의 개념
+#   - Heap은 완전이진트리로 구성된 자료구조다. 
+#   - 완전이진트리란 왼쪽을 우선적으로 채우는, 왼쪽의 트리가 전부 채워진 이진트리를 말한다.
+#   - Heap은 max Heap(최대 힙)과 min Heap(최소 힙), 두가지로 구분된다. 
+#      - max Heap은 루트노드에 항상 최대값의 요소가 위치한다. 
+#      - min Heap은 루트노드에 항상 최소값의 요소가 위치한다. 
+#   - 파이썬은 heapq 라이브러리를 통해 heap을 구현할 수 있는데, min Heap만을 지원한다. 
+
+# 2) Heap Sort 구현
+
+from typing import List
+import heapq
+
+# 최대 힙을 오름차순으로 정렬하라. 
+def heap_sort(case:List[int]) -> List[int]:
+  # min Heap을 max Heap으로 만드는 과정 
+
+  case = [-1 * n for n in case]
+  heapq.heapify(case)
+
+  sorted = [0] * len(case)
+
+  for i in range(len(case) - 1, -1, -1):
+    largest = -1 * heapq.heappop(case)
+    sorted[i] = largest
+
+  return sorted
+
+print(heap_sort(case = [3, 5, 7, 9, 4, 2]))
--- memo.py
+++ memo.py
@@ -1,6 +1,7 @@
+
 from typing import List
 
-# Bubble Sort
+# Bubble sort
 def bubble_sort(case:List[int]) -> List[int]:
   for idx in range(len(case) - 1):
     for i in range(len(case) - idx - 1):
@@ -8,7 +9,7 @@ def bubble_sort(case:List[int]) -> List[int]:
         case[i], case[i + 1] = case[i + 1], case[i]
   return case
 
-# Insertion Sort
+# Insertion sort
 def insertion_sort(case:List[int]) -> List[int]:
   for idx in range(1, len(case)):
     flag = idx - 1
@@ -16,12 +17,11 @@ def insertion_sort(case:List[int]) -> List[int]:
     while flag >= 0 and current < case[flag]:
       case[flag + 1] = case[flag]
       flag -= 1
-    case[flag + 1] = current
+    case[flag + 1] =  current
   return case
 
-
-# Selection Sort
-def selection_sort(case: List[int]) -> List[int]:
+# Selection sort
+def selection_sort(case:List[int]) -> List[int]:
   for idx in range(len(case)):
     min_num = case[idx]
     min_idx = idx
@@ -32,7 +32,7 @@ def selection_sort(case: List[int]) -> List[int]:
     case[idx], case[min_idx] = case[min_idx], case[idx]
   return case
 
-# Merge Sort
+# Merge sort
 def merge_sort(case:List[int]) -> List[int]:
   length = len(case)
   if length == 1:
@@ -42,12 +42,12 @@ def merge_sort(case:List[int]) -> List[int]:
   left_case = case[:mid]
   right_case = case[mid:]
 
-  sorted_left = merge_sort(left_case)
-  sorted_right = merge_sort(right_case)
+  sorted_left = merge_sort(case = left_case)
+  sorted_right = merge_sort(case = right_case)
 
-  sorted_case = []
   left_idx = 0
   right_idx = 0
+  sorted_case = []
 
   while left_idx < len(sorted_left) or right_idx < len(sorted_right):
     if left_idx == len(sorted_left):
@@ -63,12 +63,16 @@ def merge_sort(case:List[int]) -> List[int]:
     if sorted_left[left_idx] <= sorted_right[right_idx]:
       sorted_case.append(sorted_left[left_idx])
       left_idx += 1
+      continue
+
     else:
       sorted_case.append(sorted_right[right_idx])
       right_idx += 1
+      continue
+  
   return sorted_case
-   
-# Quick Select
+
+# Quick select
 import random
 def quick_select(case:List[int], k:int) -> int:
   length = len(case)
@@ -96,30 +100,23 @@ def quick_select(case:List[int], k:int) -> int:
       case[left_idx], case[right_idx] = case[right_idx], case[left_idx]
       continue
 
-  case[pivot_idx], case[left_idx] = case[left_idx], case[pivot_idx]
+  case[left_idx], case[last_idx] = case[last_idx], case[left_idx]
 
   if left_idx == length - k:
     return case[left_idx]
   
   if left_idx < length - k:
-    return quick_select(case = case[left_idx + 1 :], k = k)
+    return quick_select(case = case[left_idx + 1:], k = k)
   
   if left_idx > length - k:
     return quick_select(case = case[:left_idx], k = k - (length - left_idx))
-
-print(bubble_sort(case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1]))
-print(insertion_sort(case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1]))
-print(selection_sort(case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1]))
-print(merge_sort(case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1]))
-print(quick_select(case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1], k = 2))
-  
-
-# Quick Sort
+    
+# Quick sort
 def quick_sort(case:List[int], begin_idx:int, last_idx:int) -> List[int]:
   length = last_idx - begin_idx + 1
   if length <= 1:
     return case
-  
+
   pivot_idx = random.randrange(begin_idx, last_idx + 1)
   case[pivot_idx], case[last_idx] = case[last_idx], case[pivot_idx]
 
@@ -140,14 +137,13 @@ def quick_sort(case:List[int], begin_idx:int, last_idx:int) -> List[int]:
       case[left_idx], case[right_idx] = case[right_idx], case[left_idx]
       continue
 
-  case[last_idx], case[left_idx] = case[left_idx], case[last_idx]
-
+  case[left_idx], case[last_idx] = case[last_idx], case[left_idx]
   quick_sort(case = case, begin_idx = left_idx + 1, last_idx = last_idx)
   quick_sort(case = case, begin_idx = begin_idx, last_idx = left_idx - 1)
+
   return case
 
-case = [9,8,7,6,5,5,3,3,3,4,4,7,7,2,2,1,1,4,3,2,1]
+case = [5, 7, 9, 3, 1, 5, 5, 2, 4]
 quick_sort(case = case, begin_idx = 0, last_idx = len(case) - 1)
 print(case)
-
 
