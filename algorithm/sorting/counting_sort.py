a6b20d1f1847f7d18848e61886bb63004afc0f5e
devraphy <devraphy@gmail.com>
2021-10-05 17:26:18 +0900

    counting sort

--- algorithm/sorting/counting_sort.py
+++ algorithm/sorting/counting_sort.py
@@ -2,26 +2,26 @@ from typing import List
 
 def countingSort(nums:List[int])->List[int]:  
   length = len(nums)
-  min_num = min(nums)  #offset
-  max_num = max(nums)
+  min_num = min(nums) # 가장 작은 수 
+  max_num = max(nums) # 가장 큰 수 
 
-  range = max_num - min_num + 1
-  counts = [0]*range
+  range = max_num - min_num + 1 # count array를 만들기 위한 크기 설정 
+  count_array = [0] * range # count array 생성 
 
-  for num in nums:
+  for num in nums: # count array의 요소를 채우는 과정 
     count_idx = num - min_num
-    counts[count_idx] += 1
+    count_array[count_idx] += 1
 
-  acc_counts = []
-  acc_count = 0
-  for count in counts:
+  acc_counts = [] # acc(누적) array 생성 
+  acc_count = 0 # acc array의 요소가 될 값 
+  for count in count_array: # acc array의 요소를 채우는 과정 
     acc_count += count
     acc_counts.append(acc_count)
 
-  end_locs = [ c-1 for c in acc_counts]
+  end_locs = [ c-1 for c in acc_counts] # acc array - 1 배열을 만드는 과정 
 
-  sorted = [0] * length
-  for num in reversed(nums):
+  sorted = [0] * length # 최종적으로 정렬될 배열 생성 
+  for num in reversed(nums): # 맨 뒤의 요소부터 정렬을 시작하는데, 이를 리버스로 구현 
     count_idx = num - min_num
     end_loc = end_locs[count_idx]
     sorted[end_loc] = num
