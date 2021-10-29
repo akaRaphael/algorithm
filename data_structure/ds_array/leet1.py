907fb1b82b01d6b19f708b842c5eb943a707f226
devraphy <devraphy@gmail.com>
2021-10-29 17:11:46 +0900

    leet 1

---
+++ data_structure/ds_array/leet1.py
@@ -0,0 +1,16 @@
+# 1. Two Sum
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+  for i in range(len(nums)):
+    find_num = target - nums[i]
+    try: 
+      if nums.index(find_num) != i:
+        return i, nums.index(find_num)
+    except:
+      continue
+
+
+print(twoSum(nums = [3,3,1], target = 6))
+print(twoSum(nums = [-1,-2,-3,-4,-5], target = -8))
+print(twoSum(nums = [3,2,95,4,-3], target = 92))
