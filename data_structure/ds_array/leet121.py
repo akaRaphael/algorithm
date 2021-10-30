aa0d576cb82741071d6eed274667f1c893a94a62
devraphy <devraphy@gmail.com>
2021-10-30 22:34:32 +0900

    leet 121

--- data_structure/ds_array/leet121.py
+++ data_structure/ds_array/leet121.py
@@ -5,7 +5,6 @@ from typing import List
 def maxProfit(prices: List[int]) -> int:
   buy_price = prices[0]
   profit = 0
-
   for i in range(1, len(prices)):
     if prices[i] < buy_price:
       buy_price = prices[i]
