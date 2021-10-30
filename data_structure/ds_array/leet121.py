960393d6178ce1e4c72dd07788c62c728b68cada
devraphy <devraphy@gmail.com>
2021-10-30 22:31:53 +0900

    leet121

---
+++ data_structure/ds_array/leet121.py
@@ -0,0 +1,16 @@
+# 121. Best Time to Buy and Sell Stock
+
+from typing import List
+
+def maxProfit(prices: List[int]) -> int:
+  buy_price = prices[0]
+  profit = 0
+
+  for i in range(1, len(prices)):
+    if prices[i] < buy_price:
+      buy_price = prices[i]
+    else:
+      profit = max(profit, prices[i] - buy_price)
+  return profit
+  
+print(maxProfit(prices = [3,2,6,5,0,3]))  
