129d7bafa82c7085a780dacb66c433dadf7db490
devraphy <devraphy@gmail.com>
2021-11-03 17:18:34 +0900

    leet 1528

---
+++ data_structure/ds_array/leet1528.py
@@ -0,0 +1,15 @@
+# 1528. Shuffle String
+from typing import List
+
+class Solution:
+  def restoreString(self, s: str, indices: List[int]) -> str:
+
+    temp = [0] * len(indices)
+    for i in range(len(indices)):
+      temp[indices[i]] = s[i]
+    
+    return "".join(temp)
+    
+
+foo = Solution()
+print(foo.restoreString(s = "codeleet", indices = [4,5,6,7,0,2,1,3]))
