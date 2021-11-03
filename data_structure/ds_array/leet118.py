bb9323a14dd483b355e6b7615b00d25ff03a1d4e
devraphy <devraphy@gmail.com>
2021-11-03 16:29:11 +0900

    leet 118

--- data_structure/ds_array/leet118.py
+++ data_structure/ds_array/leet118.py
@@ -6,10 +6,12 @@ class Solution:
   def generate(self, numRows: int) -> List[List[int]]:
     result = []
     for i in range(numRows):
-      if i < 2:
-        result.append([1] * (i + 1))
+      if i == 0:
+        result.append([1])
+      elif i == 1:
+        result.append([1,1])
       else:
-        result.append([1] + [result[i - 1][j] + result[i - 1][j + 1] for j in range(i - 1)] + [1])
+        result.append([1]+ [result[i - 1][j] + result[i - 1][j + 1] for j in range(i - 1)] + [1])
     return result
 
 foo = Solution()
