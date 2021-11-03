01273468020bbc5f99d296a6e200eabe4b4ef7cd
devraphy <devraphy@gmail.com>
2021-11-03 15:52:43 +0900

    leet 566

--- data_structure/ds_array/leet566.py
+++ data_structure/ds_array/leet566.py
@@ -1,23 +1,24 @@
 # 566. Reshape the Matrix
 
 from typing import List
+
 class Solution:
-  def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-    originalR = len(mat)
-    originalC = len(mat[0])
-    if originalR == r or originalC == c:
-        return mat
-    elif originalR * originalC != r * c:
-        return mat
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        mat_row = len(mat)
+        mat_col = len(mat[0])
 
-    # Valid at this point
-    result = []
-    for i in range(r * c):
-        if not (i % c):
-            result.append([])
-        newItem = mat[i // originalC][i % originalC]
-        result[-1].append(newItem)
-    return result
-
+        if mat_row == r and mat_col == c:
+            return mat
+        elif mat_row * mat_col != r * c:
+            return mat
+        
+        result = []
+        for i in range(r * c):
+            if i % c == 0:
+                result.append([])
+            new_item = mat[i // mat_col][i % mat_col]
+            result[-1].append(new_item)
+        return result
+        
 foo = Solution()
 print(foo.matrixReshape(mat = [[1,2,3,4]], r = 2, c = 2))
