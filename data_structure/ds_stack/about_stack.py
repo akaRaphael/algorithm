247fba579ae34942edb00e211ad3804094507a9e
devraphy <devraphy@gmail.com>
2021-11-05 23:15:15 +0900

    stack

--- data_structure/ds_stack/about_stack.py
+++ data_structure/ds_stack/about_stack.py
@@ -9,3 +9,12 @@
 # 파이썬에서는 push 대신 append를 사용한다. 
 # pop()을 사용하면 원소가 추출되면서 동시에 제거된다. 
 # 그러므로 단순히 원소를 가져오기만 하고 싶다면 stack[-1]을 사용한다.  
+
+# 3) Stack 구현 
+
+class Stack:
+  def __init__(self):
+    self.top = [] 
+
+  def __len__(self):
+    return len(self.top)
