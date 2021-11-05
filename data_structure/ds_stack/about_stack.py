94410ba32e13b64adbaffafde7fbb216c80318f0
devraphy <devraphy@gmail.com>
2021-11-05 23:24:10 +0900

    Implementing Stack

--- data_structure/ds_stack/about_stack.py
+++ data_structure/ds_stack/about_stack.py
@@ -18,3 +18,35 @@ class Stack:
 
   def __len__(self):
     return len(self.top)
+
+  def __str__(self):
+    return str(self.top[::1])
+
+  def push(self, item):
+    self.top.append(item)
+  
+  def isEmpty(self):
+    return len(self.top) == 0
+
+  def pop(self):
+    if not self.isEmpty():
+      return self.top.pop(-1)
+    else:
+      print("Stack underflow")
+      exit()
+  
+  def clear(self):
+    self.top = []
+
+  def isContain(self, item):
+    return item in self.top
+
+  def peek(self):
+    if not self.isEmpty():
+      return self.top[-1]
+    else:
+      print("underflow")
+      exit()
+
+  def size(self):
+    return len(self.top)
