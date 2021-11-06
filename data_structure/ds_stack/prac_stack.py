82b672e0d9dd1c92e8727b34fdda24079d012cc4
devraphy <devraphy@gmail.com>
2021-11-06 11:55:36 +0900

    practicing stack implementation

---
+++ data_structure/ds_stack/prac_stack.py
@@ -0,0 +1,76 @@
+# praticing Stack implementation 
+class Stack:
+  def __init__(self):
+    self.top = []
+
+  def __len__(self):
+    return len(self.top)
+
+  def __str__(self):
+    return str(self.top[::1])
+
+  def push(self, item):
+    self.top.append(item)
+
+  def isEmpty(self):
+    return len(self.top) == 0
+
+  def pop(self):
+    if not self.isEmpty():
+      return self.top.pop(-1)
+    else:
+      print("Stack underflow")
+      exit()
+
+  def clear(self):
+    self.top = []
+
+  def isContain(self, item):
+    return item in self.top
+
+  def peek(self):
+    if not self.isEmpty():
+      return self.top[-1]
+    else:
+      print("Stack underflow")
+      exit()
+
+  def size(self):
+    return len(self.top)
+
+class Stack1:
+  def __init__(self):
+    self.top = []
+
+  def __len__(self):
+    return len(self.top)
+
+  def __str__(self):
+    return str(self.top[::1])
+
+  def push(self, item):
+    self.top.append(item)
+
+  def isEmpty(self):
+    return len(self.top) == 0
+
+  def pop(self):
+    if not self.isEmpty():
+      return self.top.pop(-1)
+    else:
+      print("Stack underflow")
+      exit()
+
+  def isContain(self, item):
+    return item in self.top
+
+  def peek(self):
+    if not self.isEmpty():
+      return self.top[-1]
+    else:
+      print("Stack underflow")
+      exit()
+
+  def size(self):
+    return len(self.top)
+
