acf11025ba01fa67e3ddef6932812089ee81fcb3
devraphy <devraphy@gmail.com>
2021-11-09 15:23:17 +0900

    leet 1615 using stack

--- data_structure/ds_stack/leet1614.py
+++ data_structure/ds_stack/leet1614.py
@@ -1,6 +1,6 @@
 # 1614. Maximum Nesting Depth of the Parentheses
 
-def maxDepth(s: str) -> int:
+def maxDepth(s: str) -> int: # 내가 풀은 방법 
   max_depth = 0
   depth = 0
   for i in range(len(s)):
@@ -15,4 +15,23 @@ print(maxDepth(s = "(1+(2*3)+((8)/4))+1"))
 print(maxDepth(s = "1"))
 print(maxDepth(s = "()"))
 print(maxDepth(s = ""))
+
+def maxDepth2(s: str) -> int: #stack을 이용한 방법 
+  max_depth = 0
+  stack = []
+  
+  for i in range(len(s)):
+    if s[i] == "(":
+      stack.append("(")
+      if len(stack) > max_depth:
+        max_depth = len(stack)
+        
+    elif s[i] == ")":
+      stack.pop()
+  return max_depth
+
+print(maxDepth2(s = "(1+(2*3)+((8)/4))+1"))
+print(maxDepth2(s = "1"))
+print(maxDepth2(s = "()"))
+print(maxDepth2(s = ""))
     
