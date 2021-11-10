cfc7773e4438106f1110bcceb7aedd5db035c57b
devraphy <devraphy@gmail.com>
2021-11-10 16:37:48 +0900

    learning about queue

--- data_structure/ds_queue/about_queue.py
+++ data_structure/ds_queue/about_queue.py
@@ -3,11 +3,13 @@
 # - Queue는 한쪽 끝에서는 삽입만 다른 끝에서는 추출만이 수행되는 자료구조다. 
 # - FIFO의 순서로 데이터가 정리되어, 가장 먼저 삽입(enqueue)된 데이터가 가장 먼저 추출(dequeue)된다.
 
+
 # 2) 파이썬에서 Queue 구현하기
 # - Queue는 리스트를 이용하여 구현 가능하지만 효율적이지 않다. 
 # - 리스트를 사용하면 pop()으로 dequeue를 구현할 수 있지만, 
 #   enqueue(새로운 데이터를 맨 앞에 추가)를 위해서는 리스트의 요소를 재정렬 해야하기 때문이다.
 # - 그러므로 collections.deque, 파이썬의 Queue 모듈, 연결리스트를 사용하는 것을 권장한다.
+
 
 # 3) List로 구현하기
 # - enqueue의 시간복잡도 O(1)
@@ -59,6 +61,7 @@ dq.append(3)
 # print(dq.popleft())
 # print(dq)
 
+
 # 5) 파이썬 queue 모듈로 구현하기 
 # - 파이썬 queue 모듈은 기본 큐(Queue), 우선순위 큐(PriorityQueue), 스택(LifoQueue)을 제공한다.
 import queue
@@ -66,5 +69,14 @@ que = queue.Queue()
 que.put(1)
 que.put(2)
 que.put(3)
-print(que.get())
-print(que.get())
+# print(que.get())
+# print(que.get())
+
+
+# 6) 연결리스트로 구현하기 (연결리스트 공부 후 구현하기)
+
+
+
+
+
+
