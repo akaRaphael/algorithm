3780c2e5fb0ac188eecebb6fafde823f7231a040
devraphy <devraphy@gmail.com>
2021-12-06 16:48:06 +0900

    practicing BST implementation

--- data_structure/ds_tree/prac_bst4.py
+++ data_structure/ds_tree/prac_bst4.py
@@ -23,25 +23,25 @@ class BST():
         else:
           self.curn.right = Node(data)
           break
-  
+        
   def search(self, data):
     self.curn = self.head
     while self.curn:
       if data == self.curn.data:
-        print(f"{data} founded.")
+        print(f"{data} found")
         return
       elif data < self.curn.data:
         self.curn = self.curn.left
       else:
         self.curn = self.curn.right
-    print(f"{data} not founded.")
-    
+    print(f"{data} not found")
+
   def delete(self, data):
-    searched = False
     self.curn = self.head
     self.parent = self.head
+    searched = False
     
-    while self.curn:
+    while True:
       if data == self.curn.data:
         searched = True
         break
@@ -53,9 +53,9 @@ class BST():
       else:
         self.parent = self.curn
         self.curn = self.curn.right
-    
+        
     if searched == False:
-      print(f"{data} not able to delete, not founded")
+      print(f"{data} not able to delete, not found")
       return
     
     if self.curn.left == None and self.curn.right == None:
@@ -84,22 +84,21 @@ class BST():
         while self.change_node.left != None:
           self.change_parent = self.change_node
           self.change_node = self.change_node.left
-        
+          
         if self.change_node.right != None:
           self.change_parent.left = self.change_node.right
-          
-        else: 
-          self.change_parent.left = None
+        else:
+          self.change_parent.right = None
           
         self.parent.left = self.change_node
         self.change_node.left = self.curn.left
         self.change_node.right = self.curn.right
-      
+        
       else:
         while self.change_node.left != None:
-            self.change_parent = self.change_node
-            self.change_node = self.change_node.left
-            
+          self.change_parent = self.change_node
+          self.change_node = self.change_node.left
+          
         if self.change_node.right != None:
           self.change_parent.left = self.change_node.right
         else:
@@ -108,29 +107,23 @@ class BST():
         self.parent.right = self.change_node
         self.change_node.left = self.curn.left
         self.change_node.right = self.curn.right
-        
-    print(f"{data} successfully deleted")
-    return True
+    print(f"{data} successfully deleted")          
   
-      
-        
-      
-    
-import random
-
+import random  
 bst = BST(0)
 bst_nums = set()
-while len(bst_nums) < 100: 
+while len(bst_nums) < 100:
   bst_nums.add(random.randint(0, 999))
-
+  
 for num in bst_nums:
   bst.insert(num)
-
+  
 delete_nums = set()
 bst_nums = list(bst_nums)
-
 while len(delete_nums) < 10:
   delete_nums.add(bst_nums[random.randint(0, 99)])
   
 for num in delete_nums:
   bst.delete(num)
+  
+  
