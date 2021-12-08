606ceb8ee1059e4addf87b44de8bd3f833f66706
devraphy <devraphy@gmail.com>
2021-12-08 14:45:19 +0900

    about list queue

--- data_structure/ds_queue/about_queue.py
+++ data_structure/ds_queue/about_queue.py
@@ -14,64 +14,28 @@
 # 3) List로 구현하기
 # - enqueue의 시간복잡도 O(1)
 # - dequeue의 시간복잡도 O(N)
-class ListQueue(object):
+class ListQueue():
   def __init__(self):
     self.queue = []
     
+  def enqueue(self, data):
+    self.queue.append(data)
+    
   def dequeue(self):
-    if len(self.queue) == 0:
-      return -1
     return self.queue.pop(0)
   
-  def enqueue(self, item):
-    self.queue.append(item)
-    pass
-  
-  def printQueue(self):
+  def print_queue(self):
     print(self.queue)
+    
+  def queue_length(self):
+    print(f"queue length = {len(self.queue)}")
   
-# list_queue = ListQueue()
-# list_queue.enqueue(1)
-# list_queue.enqueue(2)
-# list_queue.enqueue(3)
-# list_queue.printQueue()
-# print(list_queue.dequeue())
-
-
-# 4) collections.deque로 구현하기
-# - deque(데크)는 double-ended queue로 앞/뒤 방향에서 데이터를 처리할 수 있는 양방향 Queue를 의미한다.
-# - appendleft()를 사용하면 맨 앞에 데이터를 추가할 수 있다. 
-# - popleft()를 사용하면 맨 앞의 데이터를 추출할 수 있다. 
-# - 내부적으로 doubly linked list로 구현되어 있기 때문에 빠른 처리가 가능하다. 
-# - enqueue / dequeue 의 시간복잡도 O(1)
-# * deque를 사용하는 이유 - 자료구조의 시작과 끝부분에서 삽입/삭제가 빈번한 경우, 리스트보다 효율적이다! 
-
-from collections import deque
-
-dq = deque()
-dq.append(1)
-dq.append(2)
-dq.append(3)
-
-# print(dq)
-# dq.appendleft(4)
-# print(dq)
-# print(dq.pop())
-# print(dq)
-# print(dq.popleft())
-# print(dq)
-
-
-# 5) 파이썬 queue 모듈로 구현하기 
-# - 파이썬 queue 모듈은 기본 큐(Queue), 우선순위 큐(PriorityQueue), 스택(LifoQueue)을 제공한다.
-import queue
-que = queue.Queue()
-que.put(1)
-que.put(2)
-que.put(3)
-# print(que.get())
-# print(que.get())
-
-
-# 6) 연결리스트로 구현하기 (연결리스트 공부 후 구현하기)
-
+queue = ListQueue()
+queue.enqueue(0)
+queue.enqueue(1)
+queue.enqueue(2)
+queue.print_queue()
+queue.queue_length()
+queue.dequeue()
+queue.print_queue()
+queue.queue_length()
