c5ce0ae85cc55929d1cad273b729bccc607ba907
devraphy <devraphy@gmail.com>
2021-12-26 20:29:18 +0900

    pythonic lecture chapter0-1

--- programmers/pythonic/chapter0_1.py
+++ programmers/pythonic/chapter0_1.py
@@ -1,10 +1,17 @@
 test = [[1], [2]]
 test2 = [[1,2], [3,4],[5]]
 
+# 내가 짠 코드 
 def solution(mylist): 
   answer = []
-  for i in range(0, len(mylist)):
+  for i in range(len(mylist)):
     answer.append(len(mylist[i]))
   return answer
 
 print(solution(test2))
+
+# pythonic 하게 짠 코드 
+def solution2(mylist):
+  return list(map(len, mylist))
+
+print(solution2(test2))
