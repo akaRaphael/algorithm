3a7e0c88cb581808a46ec3dd5171c7f0beb4d7a1
devraphy <devraphy@gmail.com>
2021-12-31 17:33:25 +0900

    pythonic lecture chapter7_5

---
+++ programmers/pythonic/chapter7_5.py
@@ -0,0 +1,11 @@
+# 가장 큰 수, inf
+# https://programmers.co.kr/learn/courses/4008/lessons/52865
+
+# - 파이썬에는 inf라는 키워드를 지원한다.
+# - 이는 infinitive를 표현하는 키워드로, 무한대를 표현한다.
+# - 음의 무한대와 양의 무한대를 지원한다.
+
+min_val = float('-inf')
+max_val = float('inf')
+print(min_val < -10000000000)
+print(max_val > 10000000000)
