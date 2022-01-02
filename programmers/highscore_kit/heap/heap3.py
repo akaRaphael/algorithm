4845c9fabd899ebe075196285697bcbaa69143c3
devraphy <devraphy@gmail.com>
2022-01-02 18:40:37 +0900

    programmers high score kit heap3

---
+++ programmers/highscore_kit/heap/heap3.py
@@ -0,0 +1,30 @@
+# 프로그래머스 고득점 kit - heap
+# 이중우선순위큐
+# https://programmers.co.kr/learn/courses/30/lessons/42628
+
+# 입출력 예시 
+# input / return 
+# ["I 16","D 1"] / [0, 0]
+# ["I 7","I 5","I -5","D -1"] / [7, 5]
+
+operations = ["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]
+
+# 내가 짠 코드 => 통과 
+def solution(operations):
+  answer = []
+  for command in operations:
+    operator, operand = command.split(' ')
+    if operator == "I":
+      answer.append(int(operand))
+    elif operator == "D" and answer != []:
+      if operand.find("-") != -1:
+        answer.remove(min(answer))
+      else:
+        answer.remove(max(answer))
+  if len(answer) == 0:
+    answer = [0,0]
+  else:
+    answer = [max(answer), min(answer)]    
+  return answer
+
+print(solution(operations))
