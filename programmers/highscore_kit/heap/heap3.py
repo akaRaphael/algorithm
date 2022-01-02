f4706157b730ea9355033e60381e7653f012405b
devraphy <devraphy@gmail.com>
2022-01-02 21:34:46 +0900

    programmers high score kit heap3

--- programmers/highscore_kit/heap/heap3.py
+++ programmers/highscore_kit/heap/heap3.py
@@ -10,6 +10,7 @@
 operations = ["I 7","I 5","I -5","D -1"] 
 
 # 내가 짠 코드 => 통과 
+# - 그러나 문제의 출제 의도에 맞는 솔루션은 아님 
 def solution(operations):
   answer = []
   for command in operations:
@@ -27,3 +28,32 @@ def solution(operations):
   return [max(answer), min(answer)]
 
 print(solution(operations))
+
+
+# 다른사람이 작성한 코드 
+import heapq
+def solution2(arguments):
+  max_heap = []
+  min_heap = []
+  for arg in arguments:
+    if arg == "D 1":
+      if max_heap != []:
+          heapq.heappop(max_heap)
+          if max_heap == [] or -max_heap[0] < min_heap[0]:
+              min_heap = []
+              max_heap = []
+    elif arg == "D -1":
+      if min_heap != []:
+          heapq.heappop(min_heap)
+          if min_heap == [] or -max_heap[0] < min_heap[0]:
+              max_heap = []
+              min_heap = []
+    else:
+      num = int(arg[2:])
+      heapq.heappush(max_heap, -num)
+      heapq.heappush(min_heap, num)
+  if min_heap == []:
+      return [0, 0]
+  return [-heapq.heappop(max_heap), heapq.heappop(min_heap)]
+
+print(solution2(operations))
