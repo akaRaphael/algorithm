c0f6744c51eab163cb307c1f74c936c52f6c29dc
devraphy <devraphy@gmail.com>
2022-01-04 17:36:11 +0900

    programmers high score kit hash1

--- programmers/highscore_kit/hash/hash1.py
+++ programmers/highscore_kit/hash/hash1.py
@@ -22,9 +22,26 @@ def solution(participants, completion):
 
 print(solution(participants, completion))
 
-# 프로그래머스 좋아요 1등 
+# 프로그래머스에서 좋아요가 가장 많은 솔루션 
 def solution2(participants, completion):
   answer = collections.Counter(participants) - collections.Counter(completion)
   return list(answer.keys())[0]
 
 print(solution2(participants, completion))
+
+# Hash를 이용한 솔루션 
+def solution3(participants, completion):
+  hash_dict = {}
+  sum_hash = 0
+  
+  for part in participants:
+    hash_dict[hash(part)] = part
+    sum_hash += hash(part)
+    
+  for comp in completion:
+    sum_hash -= hash(comp)
+    
+  # 남은 hash 값이 완주하지 못한 선수 
+  return hash_dict[sum_hash]
+
+print(solution3(participants, completion))
