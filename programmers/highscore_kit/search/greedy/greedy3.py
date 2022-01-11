66b062e9aa89d33e0f41f6b570599bbd959f3fac
devraphy <devraphy@gmail.com>
2022-01-11 23:30:02 +0900

    programmers high score kit greedy 3

--- programmers/highscore_kit/search/greedy/greedy3.py
+++ programmers/highscore_kit/search/greedy/greedy3.py
@@ -4,15 +4,27 @@
 
 # 문자열의 순서를 지킨 상태에서 가장 큰 수를 만드는 조합을 찾아야 한다. 
 
-#내가 짠 코드 
-from itertools import permutations
 
+# ********** 내가 풀지 못한 문제 
+
+# 다른 사람의 솔루션 
 number = "4177252841"
 k = 4
 
 def solution(number, k):
+  number = list(number)
+  stack = list()
   
-  print(max(number))
+  for num in number:
+    while stack and stack[-1] < num and k > 0:
+      stack.pop()
+      k -= 1
+    else:
+      stack.append(num)
+
+  if k > 0:
+    stack = stack[:len(stack) - k]
   
-  
+  return "".join(stack)
+
 print(solution(number, k))
