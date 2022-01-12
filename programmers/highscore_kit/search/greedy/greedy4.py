3a5d5eb0967380c0150e90097712adf7f7656142
devraphy <devraphy@gmail.com>
2022-01-12 22:15:17 +0900

    programmers high score kit greedy4

--- programmers/highscore_kit/search/greedy/greedy4.py
+++ programmers/highscore_kit/search/greedy/greedy4.py
@@ -7,32 +7,34 @@
 # [70, 50, 80, 50] / 100 / 3
 # [70, 80, 50] / 100 / 3
 
-people = [70, 50, 60, 80, 50, 40, 40, 40, 40] 
+people = [70, 50, 80, 50] 
 limit = 100
 # 내가 짠 코드 
 # 최소 무게가 40키로라는 점을 이용한다.
 # 한 사람을 태우고 남은 무게가 40보다 작으면 패스 
 # 남은 무게가 40보다 크다면 남은 거에서 순회해서 남은무게보다 작거나 같은 무게를 가진 사람을 찾음
 
+import heapq
 def solution(people, limit):
-  
-  # 최소 힙 사용해서 풀어보자 
   count = 0
-  stack = list()
-  people = sorted(people, reverse=True)
-  idx = 0 
-  while idx < len(people):
-    if limit - people[idx] < 40:
-      people.pop(idx)
+  stack = []
+  heapq.heapify(people)
+  
+  while len(people) > 0:
+    curn = heapq.heappop(people)
+    if (limit - curn) < 40:
       count += 1
     else:
-      break
-      
-  mock = sum(people) // limit
-  count += mock
-  if sum(people) % limit:
+      if sum(stack) + curn > limit:
+        stack = []
+        count += 1
+        stack.append(curn)
+      else:
+        stack.append(curn)  
+        
+  if len(stack) > 0:
     count += 1
-  
+        
   return count
 
 print(solution(people, limit))
