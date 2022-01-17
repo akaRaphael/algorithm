980deca11e564418160a57b20f959fdc4d597ca0
devraphy <devraphy@gmail.com>
2022-01-17 23:54:17 +0900

    programmers high score kit binary1

--- programmers/highscore_kit/search/binary/binary1.py
+++ programmers/highscore_kit/search/binary/binary1.py
@@ -12,20 +12,20 @@ times = [7, 10]
 # 다른 사람의 풀이 
 def solution(n, times):
   answer = 0
-  left = 1 
+  left = 1 # 최소값 
   right = max(times) * n # 가장 비효율적으로 심사를 받는 경우 
-  while left <= right:
-    mid = (left + right) // 2
+  while left <= right: # 이진탐색 
+    mid = (left + right) // 2 # 중간 값 
     people = 0
-    for time in times:
-      people += mid // time
+    for time in times: 
+      people += mid // time # mid분 동안 모든 심사관이 심사한 사람의 수 
       if people >= n:
         break
     
-    if people >= n:
+    if people >= n: # 심사한 사람의 수가 n과 같거나 많은 경우 
       answer = mid
       right = mid - 1
-    elif people < n:
+    else: # 심사한 사람의 수가 n보다 적은 경우 
       left = mid + 1
   
   return answer
