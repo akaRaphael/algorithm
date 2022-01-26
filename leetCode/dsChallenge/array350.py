16130bdef1997777329f1858284305c8d2ad1747
devraphy <devraphy@gmail.com>
2022-01-26 23:41:31 +0900

    LeetCode array350

--- leetCode/dsChallenge/array350.py
+++ leetCode/dsChallenge/array350.py
@@ -14,8 +14,14 @@ from typing import List
 
 class Solution:
   def intersect(self, nums1:List[int], nums2:List[int]) -> List[int]:
+    answer = []
+    for num in nums1:
+      if num in nums2:
+        answer.append(num)
+        nums2.pop(nums2.index(num))
+        
+    return answer
       
-    
     
 
 foo = Solution()
