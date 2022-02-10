fae2d6e7e904aae4dee7fc3a1bdb20b04146a977
devraphy <devraphy@gmail.com>
2022-02-11 01:52:02 +0900

    LeetCode#350

---
+++ java_algorithm/java_ps/ps_array/IntersectionofTwoArrays2.java
@@ -0,0 +1,44 @@
+package java_algorithm.java_ps.ps_array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class IntersectionofTwoArrays2 {
+
+  public static void main(String[] args) {
+
+    IntersectionofTwoArrays2 foo = new IntersectionofTwoArrays2();
+
+    int[] nums1 = {4,9,5};
+    int[] nums2 = {4,4,9,9};
+    System.out.println(foo.intersect(nums1, nums2));
+  }
+  
+  public int[] intersect(int[] nums1, int[] nums2) {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+    List<Integer> list = new ArrayList<>();
+
+    for (int i : nums1) {
+      if(map.containsKey(i)){
+        map.put(i, map.get(i) + 1);
+      } else {
+        map.put(i, 1);
+      }
+    }
+
+    for (int i : nums2) {
+      if(map.containsKey(i) && map.get(i) >= 1){
+        list.add(i);
+        map.put(i, map.get(i) - 1);
+      } 
+    }
+    
+    return list.stream().mapToInt(e -> e).toArray();
+
+  }
+  
+}
+
+
