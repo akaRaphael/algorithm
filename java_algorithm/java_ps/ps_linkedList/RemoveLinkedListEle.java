1e290761b0859dc4c24cbc204016d32a1bd662eb
devraphy <devraphy@gmail.com>
2022-02-25 17:33:39 +0900

    LeetCode#203 in java

--- java_algorithm/java_ps/ps_linkedList/RemoveLinkedListEle.java
+++ java_algorithm/java_ps/ps_linkedList/RemoveLinkedListEle.java
@@ -12,19 +12,6 @@ public class RemoveLinkedListEle {
     }
 
     public static void main(String[] args) {
-        RemoveLinkedListEle foo = new RemoveLinkedListEle();
-
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(6);
-        head.next.next.next = new ListNode(3);
-        head.next.next.next.next = new ListNode(4);
-        head.next.next.next.next.next = new ListNode(5);
-        head.next.next.next.next.next.next = new ListNode(6);
-
-        int val = 6;
-
-        foo.removeElements(head, val);
     }
 
     public ListNode removeElements(ListNode head, int val) {
