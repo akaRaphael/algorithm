a427b16c5518e0dc35b6ab25eb0c65def2845145
devraphy <devraphy@gmail.com>
2022-02-28 16:42:26 +0900

    LeetCode#83 in java

--- java_algorithm/java_ps/ps_linkedList/LeetCode83.java
+++ java_algorithm/java_ps/ps_linkedList/LeetCode83.java
@@ -11,17 +11,6 @@ public class LeetCode83 {
          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      }
 
-    public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(1);
-        head.next.next = new ListNode(2);
-        head.next.next.next = new ListNode(3);
-        head.next.next.next.next = new ListNode(3);
-
-        LeetCode83 foo = new LeetCode83();
-        foo.deleteDuplicates(head);
-    }
-
     public ListNode deleteDuplicates(ListNode head) {
          if(head == null) {
              return head;
