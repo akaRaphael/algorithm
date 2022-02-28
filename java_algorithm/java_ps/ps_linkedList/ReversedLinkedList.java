2a4cc2d6908a80af3d204c4caa8a00c1102b094f
devraphy <devraphy@gmail.com>
2022-02-28 15:42:15 +0900

    LeetCode#206 in java

---
+++ java_algorithm/java_ps/ps_linkedList/ReversedLinkedList.java
@@ -0,0 +1,37 @@
+package java_ps.ps_linkedList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ReversedLinkedList {
+
+    //Definition for singly-linked list.
+     static class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+
+    public ListNode reverseList(ListNode head) {
+         if(head == null) {
+             return head;
+         }
+
+         ListNode prev = null;
+         ListNode curn = head;
+         ListNode nexn = head;
+         while(curn != null) {
+             nexn = nexn.next;
+             curn.next = prev;
+             prev = curn;
+             curn = nexn;
+         }
+
+         head = prev;
+         return prev;
+     }
+}
