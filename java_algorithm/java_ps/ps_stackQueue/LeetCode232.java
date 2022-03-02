f22c0f9a0f25d900f344198ef8a30795efe15f3f
devraphy <devraphy@gmail.com>
2022-03-02 17:13:13 +0900

    LeetCode#232 in java

--- java_algorithm/java_ps/ps_stackQueue/LeetCode232.java
+++ java_algorithm/java_ps/ps_stackQueue/LeetCode232.java
@@ -1,4 +1,57 @@
 package java_ps.ps_stackQueue;
 
+import java.util.Stack;
+
 public class LeetCode232 {
+
+    static class MyQueue { // Stack으로 구현
+        Stack<Integer> inputStack;
+        Stack<Integer> outputStack;
+
+        public MyQueue() {
+            inputStack = new Stack<>();
+            outputStack = new Stack<>();
+        }
+
+        public void push(int x) {
+            inputStack.push(x);
+        }
+
+        public int pop() {
+            while(!inputStack.isEmpty()) {
+                    outputStack.push(inputStack.pop());
+            }
+
+            int result = outputStack.pop();
+
+            while(!outputStack.isEmpty()) {
+                inputStack.push(outputStack.pop());
+            }
+
+            return result;
+        }
+
+        public int peek() {
+
+            if(inputStack.isEmpty()) {
+                return -1;
+            }
+
+            while(!inputStack.isEmpty()) {
+                outputStack.push(inputStack.pop());
+            }
+
+            int result = outputStack.peek();
+
+            while(!outputStack.isEmpty()) {
+                inputStack.push(outputStack.pop());
+            }
+
+            return result;
+        }
+
+        public boolean empty() {
+            return inputStack.isEmpty();
+        }
+    }
 }
