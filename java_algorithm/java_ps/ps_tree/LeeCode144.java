037e783f674e7a914181a57b7957c7156359e4a6
devraphy <devraphy@gmail.com>
2022-03-03 14:47:48 +0900

    LeetCode 144 in java

---
+++ java_algorithm/java_ps/ps_tree/LeeCode144.java
@@ -0,0 +1,42 @@
+package java_ps.ps_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeeCode144 {
+
+    // Definition for a binary tree node.
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        preOrder(root, result);
+
+        return result;
+    }
+
+    public void preOrder(TreeNode root, List<Integer> result) {
+
+        if(root == null) {
+            return;
+        }
+
+        result.add(root.val);
+
+        preOrder(root.left, result);
+        preOrder(root.right, result);
+    }
+}
