0638b28dfbf5e7ab4a8fe306c4b5d61c34a96780
devraphy <devraphy@gmail.com>
2022-03-08 23:38:39 +0900

    LeetCode 235 in java

---
+++ java_algorithm/java_ps/ps_tree/LeetCode235.java
@@ -0,0 +1,18 @@
+package java_ps.ps_tree;
+
+import java.util.Stack;
+
+public class LeetCode235 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        else if(p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+}
