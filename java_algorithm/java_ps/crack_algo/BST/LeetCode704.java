9c27d5bbfc7202e760487549cea7708cd4049a8d
devraphy <devraphy@gmail.com>
2022-03-09 14:53:23 +0900

    LeetCode704 in java

---
+++ java_algorithm/java_ps/crack_algo/BST/LeetCode704.java
@@ -0,0 +1,26 @@
+package java_ps.crack_algo.BST;
+
+public class LeetCode704 {
+
+    public int search(int[] nums, int target) {
+       int endIndex = nums.length - 1;
+       int mid = 0;
+       int start = 0;
+
+       while(start <= endIndex) {
+           mid = (start + endIndex) / 2;
+
+           if(target == nums[mid]) {
+               return mid;
+           }
+
+           if(target > nums[mid]) {
+               start = mid + 1;
+           }
+           else {
+               endIndex = mid - 1;
+           }
+       }
+       return -1;
+    }
+}
