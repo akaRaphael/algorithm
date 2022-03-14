a8cd3219b9ccd66737ec3716032a266d76d4f5b1
devraphy <devraphy@gmail.com>
2022-03-14 15:42:46 +0900

    LeetCode 977 in java

---
+++ java_algorithm/java_ps/crack_algo/TwoPointers/LeetCode977.java
@@ -0,0 +1,31 @@
+package java_ps.crack_algo.TwoPointers;
+
+import java.util.Arrays;
+
+public class LeetCode977 {
+    
+    public int[] sortedSquares(int[] nums) {
+        int len = nums.length;
+        int[] result = new int[len];
+        int start = 0;
+        int end = len - 1;
+        int idx = len - 1;
+
+        while(start <= end) {
+            int a = nums[start] * nums[start];
+            int b = nums[end] * nums[end];
+
+            if(a > b) {
+                result[idx] = a;
+                idx--;
+                start++;
+            }
+            else {
+                result[idx] = b;
+                idx--;
+                end--;
+            }
+        }
+        return result;
+    }
+}
