b76ac9bfdedaf7dc7109ce20116a8eb369c011a9
devraphy <devraphy@gmail.com>
2022-03-15 16:33:30 +0900

    LeetCode 19 in java

--- java_algorithm/java_ps/crack_algo/TwoPointers/LeetCode19.java
+++ java_algorithm/java_ps/crack_algo/TwoPointers/LeetCode19.java
@@ -6,6 +6,10 @@ public class LeetCode19 {
        ListNode head1 = head;
        ListNode head2 = head;
 
+       // 만약 n이 2라면, head1을 2칸 앞선 위치에서 시작한다.(for문)
+       // head2를 head1의 노드가 남은만큼 next로 이동시키면 2만큼 남은 위치에서 head2는 멈추게 된다.(while문)
+       // 즉, 이렇게 하면 head2.next가 지워야하는 node를 가리키게 된다. (마지막 if~else문)
+       // 이 원리를 이용하여 푸는 문제다.
        for(int i = 0; i < n; i++) {
            head1 = head1.next;
        }
