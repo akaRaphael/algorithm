4068ba93885aad46b6ba5bf6c2d84f4574dcf2af
devraphy <devraphy@gmail.com>
2022-03-15 15:02:50 +0900

    LeetCode 557 in java

---
+++ java_algorithm/java_ps/crack_algo/TwoPointers/LeetCode557.java
@@ -0,0 +1,17 @@
+package java_ps.crack_algo.TwoPointers;
+
+
+public class LeetCode557 {
+
+    public String reverseWords(String s) {
+
+        String[] array = s.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for(String word : array) {
+            StringBuilder sb = new StringBuilder(word);
+            result.append(sb.reverse() + " ");
+        }
+        return result.toString().trim();
+    }
+}
