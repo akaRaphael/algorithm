3e17a6efb0056e5b81933c7d0de995465c6edfe4
devraphy <devraphy@gmail.com>
2022-03-15 14:49:27 +0900

    LeetCode 344 in java

---
+++ java_algorithm/java_ps/crack_algo/TwoPointers/LeetCode344.java
@@ -0,0 +1,17 @@
+package java_ps.crack_algo.TwoPointers;
+
+public class LeetCode344 {
+
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+
+        while(start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
