0f807460e36ea207dd90eae1a64d2a479df95d1e
devraphy <devraphy@gmail.com>
2022-03-15 15:27:43 +0900

    LeetCode 876 in java

---
+++ java_algorithm/java_ps/crack_algo/TwoPointers/LeetCode876.java
@@ -0,0 +1,43 @@
+package java_ps.crack_algo.TwoPointers;
+
+public class LeetCode876 {
+    public ListNode middleNode(ListNode node) {
+        ListNode first = node;
+        ListNode second = node;
+
+        while(second != null && second.next != null) {
+            first = first.next;
+            second = second.next.next;
+        }
+
+        return first;
+    }
+
+//    public ListNode middleNode(ListNode node) {
+//
+//        int len = ListLength(node);
+//
+//        if(len < 2) {
+//            return node;
+//        }
+//
+//        int mid = len / 2 + 1;
+//
+//        while(mid > 0) {
+//            mid --;
+//            node = node.next;
+//        }
+//        return node;
+//    }
+//
+//    public int ListLength(ListNode node) {
+//        int count = 0;
+//
+//        while(node != null) {
+//            count++;
+//            node = node.next;
+//        }
+//
+//        return count;
+//    }
+}
