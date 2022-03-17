409339ce30f947262847571874a17afb4ba299b8
devraphy <devraphy@gmail.com>
2022-03-17 15:53:13 +0900

    TreeNode class for ps in search category

---
+++ java_algorithm/java_ps/crack_algo/search/TreeNode.java
@@ -0,0 +1,17 @@
+package java_ps.crack_algo.search;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
