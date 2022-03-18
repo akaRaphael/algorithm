0e5384b9c2c2884cac513b45ce051e3d0fc065a9
devraphy <devraphy@gmail.com>
2022-03-18 16:43:42 +0900

    LeetCode 206 in java

---
+++ java_algorithm/java_ps/crack_algo/recursion/LeetCode206.java
@@ -0,0 +1,18 @@
+package java_ps.crack_algo.recursion;
+
+public class LeetCode206 {
+
+    public ListNode reverseList(ListNode head) {
+
+        if(head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
+    }
+}
