fc0b5733b8f1e6a519127ebe08dab732c2e8883a
devraphy <devraphy@gmail.com>
2022-04-07 18:00:05 +0900

    LeetCode#1

---
+++ java_algorithm/java_ps/best75/ps_array/LeetCode1.java
@@ -0,0 +1,21 @@
+package java_ps.best75.ps_array;
+
+import java.util.HashMap;
+
+public class LeetCode1 {
+    
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                result[0] = i;
+                result[1] = map.get(target - nums[i]);
+            }
+
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+}
