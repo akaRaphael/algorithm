7b25d47d3bef8d964721485b523fb4728f5c883e
devraphy <devraphy@gmail.com>
2022-04-09 16:55:05 +0900

    extra tip was added

--- java_algorithm/java_ps/highScoreKit/java_hash/PhoneBook.java
+++ java_algorithm/java_ps/highScoreKit/java_hash/PhoneBook.java
@@ -1,5 +1,6 @@
 package java_ps.highScoreKit.java_hash;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class PhoneBook {
@@ -7,6 +8,8 @@ public class PhoneBook {
     public boolean phoneBook(String[] phone_book) {
         boolean answer = true;
         HashMap<String, Integer> map = new HashMap<>();
+
+        Arrays.sort(phone_book); // 시간 초과 나지 않기 위해서 정렬 후 찾기
 
         for(int i = 0; i < phone_book.length; i++) {
             map.put(phone_book[i], i);
